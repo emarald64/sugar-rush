8689be459f69c69d687a6eaaa0bb48e770a8dbb3
Xanderath <alex.giller@proton.me>
2026-05-07 13:02:48 -0400

    added death animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,18 +4,27 @@ const MAX_SPEED = 300.0
 const ACCEL = 600
 const JUMP_VELOCITY = -200.0
 const JUMP_ACCEL = -600.0
-const MAX_JUMP_TIME=.5
+const MAX_JUMP_TIME=.25
 const STOP_MULT=3;
 const MAX_FALL_SPEED=1000;
-const WALL_JUMP_VELOCITY=225;
+const WALL_JUMP_VELOCITY=200;
 const WALL_SLIDE_SPEED=75;
 
+@export var animating:=false
 var jumpTime:=0.0
-var animating:=false
 var started_timer:=false
 var hasJumped:=false
+@export var can_wall_jump:=true;
+
+@onready var current_cp:Node2D=$StartPos
+
+func _ready()->void:
+	$StartPos.position=global_position
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("respawn") and not animating:
+		$AnimationPlayer.play(&"death")
+	
 	if not is_on_floor():
 		#if $AnimatedSprite2D.frame==0 or jumpTime<maxJumpTime:$AnimatedSprite2D.frame=2
 		if not animating:
@@ -27,6 +36,7 @@ func _physics_process(delta: float) -> void:
 		hasJumped=false
 		started_timer=false
 		$coyoteTimer.stop()
+		$"wall jump leiency".stop()
 		$"Freeze Direction Time".stop()
 			
 	# Handle jump.
@@ -49,15 +59,24 @@ func _physics_process(delta: float) -> void:
 		var direction := Input.get_axis("move_left", "move_right")
 		#print(direction)
 		if direction:
-			if is_on_wall_only():
+			var into_wall=is_on_wall_only() and signf(get_wall_normal().x)==-signf(direction)
+			if (into_wall or not $"wall jump leiency".is_stopped()) and can_wall_jump:
 				if Input.is_action_just_pressed("jump"):
+					# wall jump
 					$"Freeze Direction Time".start()
-					velocity=Vector2(-signf(direction),-1.5)*WALL_JUMP_VELOCITY
-				else:
+					velocity=Vector2(signf(get_wall_normal().x),-2)*WALL_JUMP_VELOCITY
+				elif into_wall:
+					# wall slide
+					$"wall jump leiency".start()
 					velocity.y=minf(velocity.y,WALL_SLIDE_SPEED)
 			else:
 				velocity.x = clampf(velocity.x+(direction * ACCEL * delta * (1 if signf(direction)==signf(velocity.x) else STOP_MULT)),-MAX_SPEED,MAX_SPEED)
 		else:
 			velocity.x = move_toward(velocity.x, 0, ACCEL*delta*STOP_MULT)
 
-	move_and_slide()
+	if not animating:move_and_slide()
+
+func respawn()->void:
+	global_position=current_cp.global_position
+	$Polygon2D.scale=Vector2.ONE
+	velocity=Vector2.ZERO
